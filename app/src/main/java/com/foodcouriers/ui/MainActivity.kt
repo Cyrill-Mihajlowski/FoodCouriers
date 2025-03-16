0dc0279958cd2ddc86ba7b8014f6e12bed96708d
Cyrill Mihajlowski <dev.cyrill.mihajlowski.2002@gmail.com>
2025-03-16 19:21:54 +0300

    Initial commit.

---
+++ app/src/main/java/com/foodcouriers/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.foodcouriers.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.foodcouriers.ui.theme.FoodCouriersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodCouriersTheme { }
+        }
+    }
+}
